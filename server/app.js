cc12daadad684ac194e0c8e321ecd921153460de
kwanho <kwanho8532@gmail.com>
2026-01-07 18:25:04 +0900

    cors setting

--- server/app.js
+++ server/app.js
@@ -56,6 +56,9 @@ app.use(cors({
 // cors 미들웨어가 app.use(express.json()) 보다 위에 있어야 함
 // cors는 요청이 들어오면 라우트로 가기 전에 응답에 붙일 헤더를 미리 세팅함
 // express.json()은 요청 body를 파싱하여 req.body에 넣어줌
+
+app.options('*', cors());
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
