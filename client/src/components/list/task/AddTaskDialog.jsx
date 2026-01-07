fb2d42eef6b115e0c8ddf42ae8b0b62b56e75d30
kwanho <kwanho8532@gmail.com>
2026-01-07 19:12:11 +0900

    modify

--- client/src/components/list/task/AddTaskDialog.jsx
+++ client/src/components/list/task/AddTaskDialog.jsx
@@ -15,10 +15,12 @@ import api from '../../../api/axios';
 import { useContext } from 'react';
 import { TaskContext } from '../../../api/provider';
 import { useNavigate } from 'react-router';
+import { useAuth } from '../../../AuthContext';
 
 export default function AddTaskDialog({ user }) {
     const navigate = useNavigate()
     const [open, setOpen] = useState(false);
+    const { user } = useAuth()
 
     const { addTask } = useContext(TaskContext)
 
@@ -76,35 +78,42 @@ export default function AddTaskDialog({ user }) {
 
     return (
         <>
-            <Box sx={{ display: 'flex' }}>
-                <Button sx={{ ml: 'auto' }} variant="outlined" onClick={handleClickOpen}>
-                    Add Task
-                </Button>
-            </Box>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-                disableRestoreFocus
-            >
-                <FormProvider {...methods}>
-                    <form onSubmit={methods.handleSubmit(handleAdd)}>
-                        <DialogTitle id="alert-dialog-title">
-                            What tasks will you add?
-                        </DialogTitle>
-                        <DialogContent>
-                            <AddTaskForm />
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={handleClose} autoFocus>
-                                Cancel
-                            </Button>
-                            <Button type='submit'>Add</Button>
-                        </DialogActions>
-                    </form>
-                </FormProvider>
-            </Dialog>
+            {user ?
+                <>
+                    <Box sx={{ display: 'flex' }}>
+                        <Button sx={{ ml: 'auto' }} variant="outlined" onClick={handleClickOpen}>
+                            Add Task
+                        </Button>
+                    </Box>
+                    <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                        disableRestoreFocus
+                    >
+                        <FormProvider {...methods}>
+                            <form onSubmit={methods.handleSubmit(handleAdd)}>
+                                <DialogTitle id="alert-dialog-title">
+                                    What tasks will you add?
+                                </DialogTitle>
+                                <DialogContent>
+                                    <AddTaskForm />
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={handleClose} autoFocus>
+                                        Cancel
+                                    </Button>
+                                    <Button type='submit'>Add</Button>
+                                </DialogActions>
+                            </form>
+                        </FormProvider>
+                    </Dialog>
+                </>
+                :
+                null
+            }
+
         </>
     );
 }
